764cedfe92d5864c9ffbc9199640604c99f057d1
Ritwik Ruia <ritwikruia@gmail.com>
2017-01-20 11:07:08 -0800

    adds two more categories, religion and sports

--- routes/index.js
+++ routes/index.js
@@ -21,25 +21,8 @@ router.get('/', function(req, res, next) {
   res.render('index', {title: 'VocalKords'});
 })
 
-// test insert document on navigation to /insert
-// router.get('/insert', function(req, res, next) {
-//   var item = {
-//     name: "One",
-//     type: "Political"
-//   };
 
-//   mongo.connect(url, function(err, db) {
-//     assert.equal(null, err);
-//     db.collection('opinions').insertOne(item, function(err, result) {
-//       assert.equal(null, err);
-//       console.log('Item Inserted');
-//       db.close();
-//     });
-//   });
-//   res.redirect('/');
-// });
-
-// insert opinion
+// insert opinion Politics
 router.post('/opinion/politics', function(req, res) {
   var item = {
     category: "Politics",
@@ -66,7 +49,7 @@ router.post('/opinion/politics', function(req, res) {
     res.redirect(301, '/politics')
 })
 
-
+// Render Politics
 router.get('/politics', function(req, res) {
   console.log('oijoijouhnoiuhoiuhoiuhoiuhoihoiuhoiuhoiuhiouh');
 
@@ -97,7 +80,7 @@ router.post('/opinion/:objectid/delete', function(req, res) {
   })
 })
 
-// update an opinion
+// update an opinion Politics
 router.post('/opinion/politics/update', function(req, res) {
   var objectId = {
     title: req.body.updatetitle,
@@ -131,6 +114,154 @@ console.log(objectId);
 // router.get('/politics.html', function(req, res) {
 //   res.redirect('/politics.html');
 // })
+
+//RELIGION
+// insert opinion Religion
+router.post('/opinion/religion', function(req, res) {
+  var item = {
+    category: "Religion",
+    title: req.body.title,
+    body: req.body.body,
+    user: req.body.user,
+    upvotes: 0,
+    downvotes: 0,
+    color: randomColorPicker(colorPalette)
+  }
+  // var postTitle = req.body.title;
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    // if (db.collection('opinions').find({'title': postTitle})){}
+
+    db.collection('opinions').insertOne(item, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item Inserted');
+      db.close();
+
+    })
+  })
+    res.redirect(301, '/religion')
+})
+
+// render Religion
+router.get('/religion', function(req, res) {
+  console.log('oijoijouhnoiuhoiuhoiuhoiuhoihoiuhoiuhoiuhiouh');
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('opinions').find({"category": "Religion"}).toArray(function(err, docs) {
+    // res.json({docs}) //works
+    res.render('religion', {items: docs, test: 'aj', anotherKey: 'another value'});
+    });
+  })
+})
+
+// Update Religion
+router.post('/opinion/religion/update', function(req, res) {
+  var objectId = {
+    title: req.body.updatetitle,
+    body: req.body.updatebody,
+    id: req.body.id
+  }
+  var objid = req.body.id;
+  var objtitle = req.body.updatetitle;
+  var objbody = req.body.updatebody;
+  mongo.connect(url, function(err, db) {
+    db.collection('opinions').update( { _id: ObjectId(objid) }, {$set: {title: objtitle, body: objbody}}), function(err, upd) {
+      if (err) {
+        console.log('error!', err);
+        res.json( { status: 404} );
+      }
+      else {
+        // db.collection('data').find( { _id: ObjectId(objectId.id) } ).toArray(function(err, results) {
+        //   console.log('updated entry');
+        //   res.redirect(301, '/politics');
+        // })
+        db.close();
+      }
+    }
+        res.redirect(301, '/religion');
+  })
+console.log(objectId);
+// res.json(objectId);
+
+})
+
+//SPORTS
+// insert opinion Sports
+router.post('/opinion/sports', function(req, res) {
+  var item = {
+    category: "Sports",
+    title: req.body.title,
+    body: req.body.body,
+    user: req.body.user,
+    upvotes: 0,
+    downvotes: 0,
+    color: randomColorPicker(colorPalette)
+  }
+  // var postTitle = req.body.title;
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    // if (db.collection('opinions').find({'title': postTitle})){}
+
+    db.collection('opinions').insertOne(item, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item Inserted');
+      db.close();
+
+    })
+  })
+    res.redirect(301, '/sports')
+})
+
+// render Religion
+router.get('/sports', function(req, res) {
+  console.log('oijoijouhnoiuhoiuhoiuhoiuhoihoiuhoiuhoiuhiouh');
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('opinions').find({"category": "Sports"}).toArray(function(err, docs) {
+    // res.json({docs}) //works
+    res.render('sports', {items: docs, test: 'aj', anotherKey: 'another value'});
+    });
+  })
+})
+
+// Update Religion
+router.post('/opinion/sports/update', function(req, res) {
+  var objectId = {
+    title: req.body.updatetitle,
+    body: req.body.updatebody,
+    id: req.body.id
+  }
+  var objid = req.body.id;
+  var objtitle = req.body.updatetitle;
+  var objbody = req.body.updatebody;
+  mongo.connect(url, function(err, db) {
+    db.collection('opinions').update( { _id: ObjectId(objid) }, {$set: {title: objtitle, body: objbody}}), function(err, upd) {
+      if (err) {
+        console.log('error!', err);
+        res.json( { status: 404} );
+      }
+      else {
+        // db.collection('data').find( { _id: ObjectId(objectId.id) } ).toArray(function(err, results) {
+        //   console.log('updated entry');
+        //   res.redirect(301, '/politics');
+        // })
+        db.close();
+      }
+    }
+        res.redirect(301, '/sports');
+  })
+console.log(objectId);
+// res.json(objectId);
+
+})
+
+
+
+
 
 module.exports = router;
 
--- vkreadme.md
+++ vkreadme.md
@@ -1,45 +1,46 @@
-A readme.md file with explanations of the technologies used, the approach taken, installation instructions, unsolved problems, etc.
 
-Introduction:
+# Project 2: VocalKords
+
+### Introduction:
 
 VocalKords gives people an outlet to express their opinions in a succinct, to-the-point(hopefully) manner.
-Whether it's constructive or destructive, vehement or apathetic, cordial or accusatory, express your opinions here without fear, unless you select a username that gives you away of course.
+Whether it's constructive or destructive, vehement or apathetic, cordial or accusatory, express your opinions here without fear, unless you choose a username that gives you away of course.
 
-Technologies Used:
+#### Technologies Used:
 
-HTML5
+-HTML5
 
-CSS3
-    Materialize
+-CSS3
+    -Materialize
 
-JavaScript
-    Node.js
-    Express.js
-        Express Modules:
-        Morgan
-        Body-Parser
-        Serve-Favicon
-    jQuery
-    AJAX
+-JavaScript
+    -Node.js
+    -Express.js
+        -Express Modules:
+        -Morgan
+        -Body-Parser
+        -Serve-Favicon
+    -jQuery
+    -AJAX
 
-MongoDB
+-MongoDB
 
-Handlebars
+-Handlebars
 
-Approach:
+### Approach:
 
 The idea was to implement a combination of CSS Frameworks, Render Engines, AJAX on the front end and make use of MongoDB for CRUD operations.
 
-The site allows users to:
+#### The site allows users to:
 1) Create Opinion posts with a Body & Title
 2) Read Opinion posts, divided into categories
 3) Delete Opinion posts
 4) Update a previously posted Opinion post
 
-Installation Instructions:
+### Installation Instructions:
 1) Navigate to: https://lit-shelf-53913.herokuapp.com/
 
-Unsolved Problems/Planned Features:
+### Unsolved Problems/Planned Features:
 1) Enable more categories
 2) User login & authentication
 3) Allow users to comment on existing opinions, creating a dialog chain
